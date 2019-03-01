3c6e00c79af275743015e32f048edfbb5998ba5e
Rijnard van Tonder <rvantonder@gmail.com>
2019-03-01 01:20:52 -0500

    add license headers

--- src/interface/scheduler.mli
+++ src/interface/scheduler.mli
@@ -1,3 +1,8 @@
+(** Copyright (c) 2016-present, Facebook, Inc.
+    Modified work Copyright (c) 2018-2019 Rijnard van Tonder
+    This source code is licensed under the MIT license found in the
+    LICENSE file in the root directory of this source tree. *)
+
 open Hack_parallel_intf.Std
 
 module Daemon = Daemon
